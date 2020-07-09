f909c7c04faf66f187f36cdaf4f51b715af1565a
Denis Gorgul <davin4u@gmail.com>
2020-07-09 09:07:30 +0300

    create user command

---
+++ app/Console/Commands/CreateUserCommand.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+
+class CreateUserCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:create {--name=} {--email=} {--password=}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $name = $this->option('name');
+        $email = $this->option('email');
+        $password = $this->option('password');
+
+        if ($name && $email && $password) {
+            $user = User::where('email', $email)->first();
+
+            if (!is_null($user)) {
+                $this->error('User with given email already exists.');
+            }
+
+            $user = User::create([
+                'name' => $name,
+                'email' => $email,
+                'password' => Hash::make($password)
+            ]);
+
+            if ($user) {
+                $this->info('User successfully created.');
+
+                return;
+            }
+        }
+
+        $this->error('Something went wrong. User was not created.');
+    }
+}
