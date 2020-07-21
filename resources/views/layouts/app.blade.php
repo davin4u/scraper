36e0a671cd9d88cb1c11875a7c55dcdc1cf45c49
Olha <tykhonova.olechka@gmail.com>
2020-07-21 12:57:57 +0300

    design fixes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,90 +23,15 @@
 <body>
 <div class="container">
     <div id="app">
+        @if(\Illuminate\Support\Facades\Auth::user())
+        @include('layouts.menu')
+        @endif
 
-        <nav class="navbar navbar-expand-md navbar-light bg-white fixed-left">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
-                    aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-                <div class="container">
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse"
-                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
 
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <!-- Left Side Of Navbar -->
-                        @if(\Illuminate\Support\Facades\Auth::user())
-                            <ul class="navbar-nav mr-auto">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('categories.index') }}">Product categories</a>
-                                </li>
-
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('brands.index') }}">Product brands</a>
-                                </li>
-
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('products.index') }}">Products</a>
-                                </li>
-
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('scraper.categories.index') }}">Scraping
-                                        categories</a>
-                                </li>
-
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('scraper-jobs.index') }}">Scraping jobs</a>
-                                </li>
-                            </ul>
-                        @endif
-                    </div>
-                </div>
-            </div>
-        </nav>
-
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
-                    @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                      style="display: none;">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
-            </div>
-
-        </nav>
-
-        <main class="py-4">
+        <main class="py-4 d-md-flex">
+            @if(\Illuminate\Support\Facades\Auth::user())
+            @include('layouts.menu_sidebar')
+            @endif
             @yield('content')
         </main>
 
--- resources/views/scraper_categories/index.blade.php
+++ resources/views/scraper_categories/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-12 px-0 px-md-4">
                 <div class="card">
                     <div class="card-header clearfix">
                         <span>Scraper categories</span>
