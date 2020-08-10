e64cce427e57e614e48e4c901021fc41b25b7e70
blind3y3 <24352318+blind3y3@users.noreply.github.com>
2020-08-10 14:40:43 +0500

    add Country model and countries table

---
+++ database/migrations/2020_08_10_093526_create_countries_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('iso_code');
+            $table->string('phone_code');
+        });
+
+        DB::table('countries')->insert([
+            'name' => 'Россия',
+            'iso_code' => 'RU',
+            'phone_code' => '+7'
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
